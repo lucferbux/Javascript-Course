e4d4ab43fa79ad38d328c9a4b9dca4381aa43b88
Juan Luis Calderón <jl.calderon.b@gmail.com>
2023-01-03 15:55:17 -0300

    Corrige Aboutme.js

--- activity/1_session/aboutme.js
+++ activity/1_session/aboutme.js
@@ -1,4 +1,5 @@
-console.log("hello world");
-
-
-console.log("laater");
+let myNombreCompleto = "Juan Calderon";
+let myProfesion = "Ingeniería Industrial";
+let myPuesto = "Consultor";
+let sentenciaCompleta = `Hola, me llamo ${myNombreCompleto} y me dedico a ${myProfesion}. Estoy cursando este Master porque me gustaría trabajar como ${myPuesto}`;
+console.log(sentenciaCompleta);

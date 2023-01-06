69e634bc90e18a78c6a4c96cb07bbe6ed39b468f
MariangelaNM <mariangelanavarro10@gmail.com>
2023-01-05 20:46:36 -0600

    Primeros 2 ejer

--- activity/2_session/index.js
+++ activity/2_session/index.js
@@ -1,9 +1,33 @@
 // Ejercicio 1
 
+var person= {name: "Lucas",age: 27,profession:"Developer"};
+
+function returnKeys(obj) {
+    const keys = Object.keys(obj);//extrae todas las keys
+    console.log(keys);
+    return keys;
+}
+
+ObjeToList(person);
+
+
 // Ejercicio 2
 
 // Ejercicio 3
 
+const revertString = (weight) => {
+  if(weight=== ""){
+      throw new Error('El string es vacio!');
+  }else{
+    let stringRev ="";
+      for(let i= 0; i<weight.length; i++){
+          stringRev = weight[i]+stringRev;
+      }
+      console.log(stringRev) ;
+  }
+};
+
+revertString("we");
 // Ejercicio 4
 
 // Ejercicio 5

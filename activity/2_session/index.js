0f0ade735d201535655488358b710f96948cdbfb
MariangelaNM <mariangelanavarro10@gmail.com>
2023-01-06 21:03:39 -0600

    Desarrollo de ejercicio 4

--- activity/2_session/index.js
+++ activity/2_session/index.js
@@ -1,11 +1,11 @@
 // Ejercicio 1
 
-var person= {name: "Lucas",age: 27,profession:"Developer"};
+var person = { name: "Lucas", age: 27, profession: "Developer" };
 
 function returnKeys(obj) {
-    const keys = Object.keys(obj);//extrae todas las keys
-    console.log(keys);
-    return keys;
+  const keys = Object.keys(obj);//extrae todas las keys
+  console.log(keys);
+  return keys;
 }
 
 ObjeToList(person);
@@ -15,33 +15,49 @@ ObjeToList(person);
 
 // Ejercicio 3
 
-const revertString = (weight) => {
-  if(weight=== ""){
-      throw new Error('El string es vacio!');
-  }else{
-    let stringRev ="";
-      for(let i= 0; i<weight.length; i++){
-          stringRev = weight[i]+stringRev;
-      }
-      console.log(stringRev) ;
+var cadenaInvertir = "";
+
+const revertString = (string) => {
+  if (string === "") {
+    throw new Error('El string es vacio!');
+  } else {
+    let stringRev = "";
+    for (let i = 0; i < string.length; i++) {
+      stringRev = string[i] + stringRev;
+    }
+    return (stringRev);
   }
 };
 
-revertString("we");
+const invertirCadena = {
+  cadena: revertString(cadenaInvertir)
+};
+
+console.log(invertirCadena);
+
 // Ejercicio 4
+
+let username = "admin";
+let password = "passwd";
+
+let login = (username == "admin" && password == "passwd") ?
+  () => alert('User logged in') :
+  () => alert('User or passwd incorrect');
+
+login(username, password);
 
 // Ejercicio 5
 
 // Ejercicio 6
 
 let loginWitUsername = (username, password) => {
-    return new Promise(function (resolve, rejected) {
-      setTimeout(() => {
-        if (username === "admin" && password === "passwd") {
-          resolve("User logged in");
-        } else {
-          rejected("Error: invalid username or password");
-        }
-      }, 200);
-    });
+  return new Promise(function (resolve, rejected) {
+    setTimeout(() => {
+      if (username === "admin" && password === "passwd") {
+        resolve("User logged in");
+      } else {
+        rejected("Error: invalid username or password");
+      }
+    }, 200);
+  });
 };

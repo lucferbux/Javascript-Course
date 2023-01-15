c42b56a4bac410a602c711b3d4f6074343c67464
MariangelaNM <mariangelanavarro10@gmail.com>
2023-01-14 20:15:46 -0600

    Se realiza el eje 2

---
+++ activity/3_session/index.ts
@@ -0,0 +1,43 @@
+
+//-----------------------------E1----------------------------------------
+// 'name', 'age' y 'profession'
+interface PersonData {
+  name: string;
+  age: number;
+  profession: string;
+}
+
+
+let getPersonDetail = (person: PersonData) => {
+  let list = [person.name, person.age, person.profession];
+  return list;
+}
+
+
+let Info = {
+  name: 'Mariangela',
+  age: 22,
+  profession: "developer",
+};
+
+console.log(getPersonDetail(Info));
+
+//--------------------------E2---------------------------------
+let x = 4;
+let y = "Mundo";
+
+function sumOrConcatenate(x: any, y: any) {
+  try {
+    if ((typeof x) == "string" && (typeof y) == "string") {
+      console.log(x + "-" + y);
+    } else if ((typeof x) == "number" && (typeof y) == "number") {
+      console.log(x + y);
+    } else {
+      console.log(x + ":" + y);
+    }
+  } catch  {
+    console.log("Error");
+  }
+}
+
+sumOrConcatenate(x, y);

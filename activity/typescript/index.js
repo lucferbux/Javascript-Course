0fd7a2d6388b70a429a22759a7956a268fa7111b
Juan Luis Calderón <jl.calderon.b@gmail.com>
2023-01-24 17:49:48 -0300

    Typescript Ejercicio 1 y 2

--- activity/typescript/index.js
+++ activity/typescript/index.js
@@ -1,4 +1,25 @@
 // Ejercicio 1
+function toArray(persona) {
+    var perfil = [persona.name, persona.age, persona.profession];
+    return perfil;
+}
+var humano = {
+    name: "Juan",
+    age: 48,
+    profession: "Ingeniero"
+};
+console.log(toArray(humano));
 // Ejercicio 2
+function sumOrConcatenate(param1, param2) {
+    if (typeof param1 === "string" && typeof param2 === "string")
+        return param1 + "-" + param2;
+    if (typeof param1 === "number" && typeof param2 === "number")
+        return param1 + param2;
+    if ((typeof param1 === "string" && typeof param2 === "number") ||
+        (typeof param1 === "number" && typeof param2 === "string"))
+        return param1 + ":" + param2;
+    return "Tipo de alguno de los parámetros INVÁLIDO";
+}
+console.log(sumOrConcatenate("7", "5"));
 // Ejercicio 3
 // Ejercicio 4
--- activity/typescript/index.ts
+++ activity/typescript/index.ts
@@ -1,6 +1,44 @@
 // Ejercicio 1
 
+interface Person {
+    name: string;
+    age: number;
+    profession: string;
+} 
+
+function toArray(persona:Person): Array<number|string>  {
+    let perfil= [persona.name, persona.age, persona.profession];
+return perfil;
+
+}
+
+const humano : Person = {
+    name : "Juan",
+    age : 48,
+    profession : "Ingeniero"
+}
+
+console.log(toArray(humano));
+
 // Ejercicio 2
+
+function sumOrConcatenate(param1:number|string, param2:number|string):number|string  {
+
+    if (typeof param1 === "string" && typeof param2 === "string")
+       return param1+"-"+param2;
+ 
+    if (typeof param1 === "number" && typeof param2 === "number")
+       return param1 + param2;
+ 
+    if ((typeof param1 === "string" && typeof param2 === "number") || 
+        (typeof param1 === "number" && typeof param2 === "string"))
+       return param1+":"+param2;
+ 
+ return "Tipo de alguno de los parámetros INVÁLIDO";
+ 
+ }
+ 
+ console.log(sumOrConcatenate("7","5"));
 
 // Ejercicio 3
 

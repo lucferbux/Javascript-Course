bb5f15f3ffbcef5563bd6c1df4b0a07b087b81f0
Lucas Fernandez <lucasfernandezaragon@gmail.com>
2023-01-30 20:04:56 +0100

    update iterables

--- advanced/11_iterables/index.js
+++ advanced/11_iterables/index.js
@@ -5,12 +5,14 @@ console.log("%c1. map", "color: #229179; font-size: 16px;");
 
 const iterableArrayMap = [1, 2, 3];
 const newArrayMap = iterableArrayMap.map((number) => number + 1);
+console.log(newArrayMap);
 
 console.log("%c1. filter", "color: #229179; font-size: 16px;");
 // filter([🍿, 🍔, 🍳], isVegetarian) =>  [🍿, 🍳]
 
 const iterableArrayFilter = [10, 20, 30, 40];
 const filteredValues = iterableArrayFilter.filter((number) => number > 20);
+console.log(filteredValues);
 
 console.log("%c1. reduce", "color: #229179; font-size: 16px;");
 // reduce([🍿, 🍳], eat) => 💩
@@ -19,12 +21,16 @@ const interableArrayReduce = [10, 20, 30];
 const total = interableArrayReduce.reduce((accumulator, number) => {
   return accumulator + number;
 });
+console.log(total);
 
 console.log("%c1. foreach", "color: #229179; font-size: 16px;");
 // map map([🌽, 🐮, 🐔], cook) => [🍿, 🍔, 🍳]
 
 const iterableArrayForEach = [1, 2, 3];
-const sumNumbers = (numbers) => number + 1;
+const sumNumbers = (number) => {
+  console.log(number + 1);
+};
 const newArrayForEach = iterableArrayForEach.forEach((number) =>
   sumNumbers(number)
 );
+console.log(newArrayForEach);

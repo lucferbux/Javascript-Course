93624aab1bd4c4758f273d785157cffd85efa80a
mpcevallos <mpcevallos@gmail.com>
2023-05-13 21:44:59 -0500

    Actividad de Sesion 1

--- activity/1_session/aboutme.js
+++ activity/1_session/aboutme.js
@@ -1,4 +1,12 @@
 console.log("hello world");
 
+// declaración y valor de variables 
+let name1 = "Maria Paula Cevallos";
+let profession1 = "Web Designer";
+let position1 = "Tech Lead";
 
-console.log("laater");
+// declaración de la variable biografia 
+let biografia = `Hola, me llamo ${name1} y me dedico a ${profession1}. Estoy cursando este Máster porque me gustaría trabajar en ${position1}`;
+
+// imprime el resultado de biografia
+console.log(biografia);
--- activity/1_session/index.js
+++ activity/1_session/index.js
@@ -1,14 +1,40 @@
 // Ejercicio 1
-import "./aboutme.js";
+
+import "../1_session/aboutme.js";
 console.log("Ejercicio 1");
 
 // Ejercicio 2
+console.log("New Breakpoint");
 
 // Ejercicio 3
 
 // Ejercicio 4
 
+// Itera del número 1 al 100
+for (let i = 1; i <= 100; i++) {
+    // Comprueba si el número actual es divisible por 7
+        if (i % 7 === 0) {
+    // Si el número es divisible por 7 lo debe imprimir en la consola.
+          console.log(i);
+        }
+      }
+
 // Ejercicio 5
 
 // Ejercicio 6
 
+// Objeto, metodo append y atributo prototype
+
+let formatter = {
+    prefix: "Hello ",
+    append: function(str) {
+      console.log(this.prefix + str);
+    }
+  };
+  
+  formatter.__proto__.toLowerString = function(str) {
+    console.log(str.toLowerCase());
+  };
+  
+  formatter.append("World"); // Resultado: Hello World
+  formatter.toLowerString("I'm Lucas"); // Resultado: i'm lucas

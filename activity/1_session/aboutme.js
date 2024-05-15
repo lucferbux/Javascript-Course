2fd482ce88e891b7790d57152d060e88d03ff5f5
joseML2020 <josemoroluque@gmail.com>
2024-05-15 20:31:37 +0200

    Updated aboutme.js to export a named function aboutMe; imported and logged its return value in index.js

--- activity/1_session/aboutme.js
+++ activity/1_session/aboutme.js
@@ -1,4 +1,20 @@
-console.log("hello world");
+const me = {
+    'name': 'Jose Moro',
+    'profession': 'Developer',
+    'position': 'Full Stack',
+}
 
 
-console.log("laater");
+/**
+ * Get message about me
+ *
+ * @returns {string}
+ */
+function aboutMe() {
+    return `Hola, me llamo ${me.name} y me dedico a ${me.profession}.
+     Estoy cursando este Máster porque me gustaría trabajar en ${me.position}`
+}
+
+export {
+    aboutMe
+}

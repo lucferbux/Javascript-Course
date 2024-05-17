aa94a93a520e3557c5ff543635c00ea811f5bb50
Jachacon12 <jorgech.webdeveloper@gmail.com>
2024-05-17 16:24:13 +0200

    feat: funcion para crear una oracion utilizando template literals

--- activity/1_session/aboutme.js
+++ activity/1_session/aboutme.js
@@ -1,4 +1,11 @@
-console.log("hello world");
+/**
+ * Creates a sentence introducing a person.
+ *
+ * @param {string} name - The name of the student.
+ * @param {string} title - The title or degree of the student.
+ * @param {string} job - The job or profession the student aspires to.
+ * @returns {string} The introduction sentence.
+ */
 
+export const createSentece = (name, title, job) => `Hola, me llamo ${name} y me dedico a ${title}. Estoy cursando este Máster porque me gustaría trabajar en ${job}`
 
-console.log("laater");

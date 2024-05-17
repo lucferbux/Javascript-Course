25e8bfd3b1c3331d2ebcd09ae52e03ebbc5a039a
Jachacon12 <jorgech.webdeveloper@gmail.com>
2024-05-17 16:24:40 +0200

    feat: Objecto con variables requeridas

--- activity/1_session/aboutme.js
+++ activity/1_session/aboutme.js
@@ -9,3 +9,8 @@
 
 export const createSentece = (name, title, job) => `Hola, me llamo ${name} y me dedico a ${title}. Estoy cursando este Máster porque me gustaría trabajar en ${job}`
 
+export const fakeStudent = {
+    name: 'John Doe',
+    title: 'Enfermero',
+    job: 'Doctor'
+}

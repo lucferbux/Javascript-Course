b4a2c16e1ef477e0113328cf80465aaf842c3abf
Jachacon12 <jorgech.webdeveloper@gmail.com>
2024-05-17 18:20:08 +0200

    feat: ejercicio 6 objeto formatter y atributo prototype

--- activity/1_session/index.js
+++ activity/1_session/index.js
@@ -1,29 +1,31 @@
 // Ejercicio 1
-import { createSentece, fakeStudent } from "./aboutme.js";
+import { createSentece, fakeStudent } from './aboutme.js';
 const { name, title, job } = fakeStudent;
 
-const fakeSentence = createSentece(name, title, job)
+const fakeSentence = createSentece(name, title, job);
 
-const sentence = createSentece('Jorge Chacon', 'Frontend Developer', 'Full-Stack Developer - Growth Specialist');
+const sentence = createSentece(
+  'Jorge Chacon',
+  'Frontend Developer',
+  'Full-Stack Developer - Growth Specialist'
+);
 
 console.log(sentence);
 console.log(fakeSentence);
 
-console.log("Ejercicio 1");
-
+console.log('Ejercicio 1');
 
 // Ejercicio 2
 
 // Ejercicio 3
 
 const divisiblesDeSiete = () => {
-    let result = []
-    for (let numero = 0; numero < 101; numero++) {
-        
-        if(numero % 7 === 0) result.push(numero)
-    }
-    return result
-}
+  let result = [];
+  for (let numero = 0; numero < 101; numero++) {
+    if (numero % 7 === 0) result.push(numero);
+  }
+  return result;
+};
 
 console.log(divisiblesDeSiete());
 
@@ -32,4 +34,33 @@ console.log(divisiblesDeSiete());
 // Ejercicio 5
 
 // Ejercicio 6
+
+// Crear el objeto formatter
+const formatter = {
+  prefix: 'Hello ',
+  append: function (str) {
+    console.log(this.prefix + str);
+  },
+};
+
+// Usar el método append
+formatter.append('World');
+
+// Añadir un nuevo método al prototype de formatter
+ formatter.__proto__.toLowerString = function (str) {
+   console.log(str.toLowerCase());
+ };
+
+// Usar el método toLowerString
+ formatter.toLowerString('I’m Jorge');
+
+const baseFormatter = {
+  ...formatter,
+  toLowerString: function (str) {
+    console.log(str.toLowerCase());
+  },
+};
+
+baseFormatter.toLowerString('I’m Jorge');
+
 

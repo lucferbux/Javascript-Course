6f36d20d426ddf94e464c53a20f6ab8b6331fcdc
Chris-AM <c.aranguizm@outlook.com>
2024-05-19 12:55:34 -0400

    :sparkles: Finaliza los 6 ejercicios de la primera semana.

--- activity/1_session/aboutme.js
+++ activity/1_session/aboutme.js
@@ -1,4 +1,9 @@
 console.log("hello world");
 
+const fullName = "Christian Aránguiz Morales";
+const profession = "Software Engineer";
+const occupation = "Full Stack Developer";
+
+console.log(`Hola, me llamo ${fullName} y me dedico a ${profession}. Estoy cursando este Máster porque me gustaría trabajar en ${occupation}.`);
 
 console.log("laater");

657cfc0880cc5f85c22659af535d889fa233cc76
Jachacon12 <jorgech.webdeveloper@gmail.com>
2024-05-27 16:49:10 +0200

    feat: ejercicio 1 - arrow function

--- activity/2_session/index.js
+++ activity/2_session/index.js
@@ -1,4 +1,32 @@
 // Ejercicio 1
+// Example of arrow function
+const person = {
+    name: 'Lucas',
+    age: 27,
+    profession:'Developer',
+};
+
+// Regresa las llaves de un objeto
+const getKeys = (obj) => Object.keys(obj).map(key => key)
+
+// Regresa las llaves y su respectivo valor
+
+const getAllInfo = (obj) => Object.keys(obj).map(key => `${key}: ${obj[key]}`);
+
+
+// Hace la misma funcion que getAllInfo pero puede usarse cuando manipulamos objetos grandes de un forma mas optimizada
+const getData = (obj) => {
+    let result = [];
+    for (let key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            result.push(`${key}: ${obj[key]}`);
+        }
+    }
+    return result;
+};
+
+
+console.log(getData(person));
 
 // Ejercicio 2
 

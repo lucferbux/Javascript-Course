e3583e421c6868a207c24778828b7d61e40c41e3
Jachacon12 <jorgech.webdeveloper@gmail.com>
2024-05-27 19:59:21 +0200

    feat: clase para logear usuario

--- activity/2_session/script.js
+++ activity/2_session/script.js
@@ -1,4 +1,3 @@
-
 export class InvertirCadena {
   constructor() {
     this.cadenaInvertir = '';
@@ -12,4 +11,29 @@ export class InvertirCadena {
     const cadenaInvertida = this.cadenaInvertir.split('').reverse().join('');
     return cadenaInvertida;
   };
+}
+
+/**
+ * Login class handles the login functionality.
+ */
+export class Login {
+  /**
+   * Constructor for the Login class.
+   * @param {string} username - The username of the user.
+   * @param {string} password - The password of the user.
+   */
+  constructor(username, password) {
+    this.username = username;
+    this.password = password;
+  }
+  /**
+   * Method to handle the login functionality.
+   * If the username is 'admin' and password is 'passwd', it alerts 'Usuario logeado'.
+   * Otherwise, it alerts 'Usuario o contraseña incorrectas'.
+   */
+  login() {
+    if (this.username === 'admin' && this.password === 'passwd')
+      alert('Usuario logeado');
+    else alert('Usuario o contraseña incorrectas');
+  }
 }

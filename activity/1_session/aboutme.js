2e61654cb128e49226d6914251bde186a8aa4cf1
Cesar Ibarra <cesarij07@gmail.com>
2024-06-10 15:49:30 -0600

    feat(index.js): update exercises

--- activity/1_session/aboutme.js
+++ activity/1_session/aboutme.js
@@ -10,6 +10,7 @@ const myName = "César Ibarra";
 const myProfession = "Frontend Developer";
 const myPosition = "Fullstack Developer";
 
-const aboutMe = `Hola, me llamo ${myName} y me dedico a ${myProfession}. Estoy cursando este Máster porque me gustaría trabajar en ${myPosition}`
+const aboutMe = `Hola, me llamo ${myName} y me dedico a ${myProfession}. Estoy cursando este Máster porque me 
+gustaría trabajar en ${myPosition}`
 
 export { aboutMe };

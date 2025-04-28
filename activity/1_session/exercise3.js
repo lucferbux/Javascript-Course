26747b452f91b3e50e004a13482b4ce61ffbbf73
Carlos Vargas <carlos.vargas@eccocar.com>
2025-04-27 23:32:26 -0500

    Adding script for exercise 3

--- activity/1_session/exercise3.js
+++
@@ -1,5 +0,0 @@
-for (let i = 1; i <= 100; i++) {
-    if (i % 7 === 0) {
-      console.log(i);
-    }
-}
--- activity/1_session/index.js
+++ activity/1_session/index.js
@@ -5,6 +5,11 @@ console.log("Ejercicio 1");
 // Ejercicio 2
 
 // Ejercicio 3
+for (let i = 1; i <= 100; i++) {
+    if (i % 7 === 0) {
+      console.log(i);
+    }
+}
 
 // Ejercicio 4
 

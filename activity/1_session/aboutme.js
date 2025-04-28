6519e95194292c7202147ccc648e82509f706d36
Carlos Vargas <carlos.vargas@eccocar.com>
2025-04-27 23:03:22 -0500

    Adding exercise 1 in index.js file

--- activity/1_session/aboutme.js
+++ activity/1_session/aboutme.js
@@ -1,4 +1,7 @@
-console.log("hello world");
+const nombreCompleto = "Carlos Vargas";
+const profesionActual = "Desarrollador Back End";
+const puestoDeseado = "Desarrollador Full Stack";
 
+const presentacion = `Hola, me llamo ${nombreCompleto} y me dedico a ${profesionActual}. Estoy cursando este Máster porque me gustaría trabajar en ${puestoDeseado}`;
 
-console.log("laater");
+console.log(presentacion);
